deafd1d82faa1c7a3de6b494bab8e0f95e1a87fa
itserg2008 <sergeypk2008@gmail.com>
2018-10-30 22:53:08 +0300

    Transferring editor config

--- src/app/home/notes-list-with-editor/notes-list-with-editor.component.ts
+++ src/app/home/notes-list-with-editor/notes-list-with-editor.component.ts
@@ -4,6 +4,8 @@ import { DomSanitizer } from '@angular/platform-browser';
 import { ActivatedRoute } from "@angular/router";
 import { LocalNoteService } from '../../services/local-note-service';
 import { Note } from '../../model/note';
+import { Folder } from '../../model/folder';
+import { LinkTreeItem } from '../../model/ui/linktreeitem';
 
 @Component({
   selector: 'app-notes-list-with-editor',
@@ -12,6 +14,8 @@ import { Note } from '../../model/note';
   host: { "style": "height:100%; display: flex; flex-direction:column" }
 })
 export class NotesListWithEditorComponent implements OnInit {
+
+  INTERNAL_LINK_PREFIX = 'local:';
 
   public searchFor: string;
   notes = Array<Note>();
@@ -33,7 +37,32 @@ export class NotesListWithEditorComponent implements OnInit {
       }
     })
 
+    const parent = this;
     this.editorSetup = {
+      plugins: 'paste link anchor toc searchreplace table code codesample lists print textcolor',
+      menubar: false,
+      statusbar: false,
+      branding: false,
+      toolbar: 'print | insert | undo redo | formatselect | bold italic underline backcolor forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | table | code codesample',
+      link_list: async function (success) {
+        const list = await parent.prepareLinkSelectionMenu()
+        success(list)
+      },
+      codesample_languages: [
+        {text: 'HTML/XML', value: 'markup'},
+        {text: 'JavaScript', value: 'javascript'},
+        {text: 'bash',value: 'bash'},  
+        {text: 'JSON',value: 'json'},
+        {text:"go",value:"go"},         
+        {text: 'CSS', value: 'css'},
+        {text: 'PHP', value: 'php'},
+        {text: 'Ruby', value: 'ruby'},
+        {text: 'Python', value: 'python'},
+        {text: 'Java', value: 'java'},
+        {text: 'C', value: 'c'},
+        {text: 'C#', value: 'csharp'},
+        {text: 'C++', value: 'cpp'}
+      ],      
       paste_data_images: true, setup: editor => {
         this.noteEditor = editor;
       }
@@ -50,6 +79,67 @@ export class NotesListWithEditorComponent implements OnInit {
     const avatarsSafeUrl = sanitizer.bypassSecurityTrustResourceUrl('./assets/avatars.svg');
 
     iconRegistry.addSvgIconSetInNamespace('avatars', avatarsSafeUrl);
+  }
+
+  async readFolderToLinkSelectionMenu(folder: Folder, items: LinkTreeItem[]) {
+    if (folder.notes != null) {
+        for (const note of folder.notes) {
+          const link = new LinkTreeItem(note.title)
+          link.value = this.INTERNAL_LINK_PREFIX + note.id;
+          items.push(link);
+        }
+    }
+
+    if (folder.children != null) {
+        for (const childFolder of folder.children) {
+          const link = new LinkTreeItem(childFolder.title);
+          link.menu = new Array<LinkTreeItem>();
+          await this.readFolderToLinkSelectionMenu(childFolder, link.menu);
+          items.push(link);
+        }
+    }
+
+  }
+
+  async prepareLinkSelectionMenu(): Promise<LinkTreeItem[]> {
+    let items = new Array<LinkTreeItem>();
+    const rootFolder = await this.buildTreeFromNodesList();
+    await this.readFolderToLinkSelectionMenu(rootFolder, items)
+    return items
+  }  
+
+  async buildTreeFromNodesList() : Promise<Folder> {
+    const folders = await this.localNoteService.getAllFolders(false)
+    const notes = await this.localNoteService.getAllNotes(false)
+
+    folders.sort((a, b) => a.level - b.level)
+    notes.sort((a, b) => a.level - b.level)
+  
+    const root = folders[0]
+    const foldersMap = new Map<string, Folder>();
+    foldersMap.set(root.id, root)
+
+    for(let i = 1; i < folders.length; i++) {
+      const folder = folders[i];
+      const parentFolder = foldersMap.get(folder.parentId)
+      
+      if (parentFolder.children == null) {
+          parentFolder.children = Array<Folder>();
+      }
+      parentFolder.children.push(folder)
+      foldersMap.set(folder.id, folder)
+    }
+
+    for(const note of notes) {
+      const parentFolder = foldersMap.get(note.folderId)
+
+      if (parentFolder.notes == null) {
+        parentFolder.notes = Array<Note>();
+      }                 
+      parentFolder.notes.push(note)      
+    }
+  
+    return Promise.resolve(root)
   }
 
   async loadAllNotes() {
