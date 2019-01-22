e1f3bb94833517193af993f760b90c71ab3bcd9b
itserg2008 <sergeypk2008@gmail.com>
2019-01-22 18:36:29 +0300

    Remove unused dependencies

--- src/app/providers/electron.service.ts
+++ src/app/providers/electron.service.ts
@@ -2,33 +2,18 @@ import { Injectable } from '@angular/core';
 
 // If you import a module but never use any of the imported values other than as TypeScript types,
 // the resulting javascript file will look as if you never imported the module at all.
-import { ipcRenderer } from 'electron';
 import { remote } from 'electron';
-
-import * as childProcess from 'child_process';
 
 @Injectable()
 export class ElectronService {
 
-  ipcRenderer: typeof ipcRenderer;
-  childProcess: typeof childProcess;
   remote: typeof remote;
 
   constructor() {
-    // Conditional imports
-    if (this.isElectron()) {
-      this.ipcRenderer = window.require('electron').ipcRenderer;
       this.remote = window.require('electron').remote;
-      this.childProcess = window.require('child_process');
-    }
-  }
-
-  isElectron = () => {
-    return window && window.process && window.process.type;
   }
 
   getUserDataPath() : string {
     return this.remote.app.getPath('userData')
   }
-
 }
