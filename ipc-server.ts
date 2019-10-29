c9b83785c9fc9d564ff38f0a34374defd2f52b67
itserg2008 <sergeypk2008@gmail.com>
2019-10-29 22:53:55 +0300

    Adding Storage IPC

--- ipc-server.ts
+++ ipc-server.ts
@@ -1,14 +1,29 @@
-import { Manager, SyncService } from 'giganotes-core';
+import { Manager, SyncService, Storage } from 'giganotes-core';
 import { ipcMain } from 'electron';
 
 export function initIpc(manager: Manager) {
   initSyncServiceIpc(manager.syncService);
+  initStorageIpc(manager.storage);
 }
 
 function initSyncServiceIpc(syncService: SyncService) {
-  ipcMain.on('sync-service-sync-request', (event, arg) => {
-    syncService.doSync(arg['userId']).then(() => {
-        event.reply('sync-service-sync-reply', {});
+    ipcMain.on('sync-service-sync-request', (event, arg) => {
+        syncService.doSync(arg['userId']).then(() => {
+            event.reply('sync-service-sync-reply', {});
+        });
     });
-});
+}
+
+function initStorageIpc(storage: Storage) {
+  ipcMain.on('storage-get-request', (event, arg) => {
+      storage.get(arg).then(value => {
+          event.reply('storage-get-reply', value);
+      });
+  });
+
+  ipcMain.on('storage-set-request', (event, arg) => {
+    storage.set(arg.key, arg.value).then(value => {
+      event.reply('storage-set-reply', null);
+    });
+  });
 }
--- src/app/services/storage.ts
+++ src/app/services/storage.ts
@@ -1,3 +1,4 @@
+import { ipcRenderer } from 'electron';
 import { Injectable } from '@angular/core';
 
 @Injectable()
@@ -5,10 +6,29 @@ export class Storage {
 
 
     async get(key: string): Promise<any> {
-        return null;
+      const promise = new Promise(function (resolve, reject) {
+        ipcRenderer.once('storage-get-reply', (event, arg) => {
+            resolve(arg);
+        });
+      });
+
+      ipcRenderer.send('storage-get-request', key);
+      return promise;
     }
 
     async set(key: string, value: any): Promise<void> {
+      const promise = new Promise<void>(function (resolve, reject) {
+        ipcRenderer.once('storage-set-reply', (event, arg) => {
+            resolve();
+        });
+      });
+
+      ipcRenderer.send('storage-set-request', {
+        key: key,
+        value: value
+      });
+
+      return promise;
     }
 
     async remove(key: string): Promise<void>  {
